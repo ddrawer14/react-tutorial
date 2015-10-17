acd95be1af19ba10b1989877a014d5ea222ef7f0
ddrawer14 <ddrawer14@yahoo.com>
2015-10-17 14:00:33 -0400

    react-tutorial complete

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,17 +1,54 @@
 
-var data = [
-	{author: "David R", text: "This is my first comment"},
-	{author: "Person 2", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
+
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	handleCommentSubmit: function(comment) {
+		var comments = this.state.data;
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+		
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	getInitialState: function() {
+		return { data: [] };
+	},
+
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+
 	render: function() {
 		return (
-			<div className = "commentBox"> 
-				Hello World! I am a CommentBox.
-				<h1> Comments </h1>
-				<CommentList data={this.props.data} />
-				<CommentForm />
+			<div className = "commentBox"> 				
+				<h1> Comments Box </h1>
+				<CommentList data={this.state.data} />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 		);
 	}
@@ -37,8 +74,8 @@ var CommentList = React.createClass({
 var Comment = React.createClass({
 	rawMarkup: function() {
 		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-		return{ __html: rawMarkup };
-	}
+		return { __html: rawMarkup };
+	},
 
 	render: function() {
 		return (
@@ -53,16 +90,32 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = this.refs.author.value.trim();
+		var text = this.refs.text.value.trim();
+		
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.refs.author.value = '';
+		this.refs.text.value = '';
+		return;
+	},
+
 	render: function() {
 		return (
-			<div className = "commentForm">
-				Hello world! I am a CommentForm.
-			</div>
+			<form className = "commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Your name" ref="author" />
+				<input type="text"	placeholder="Say something..." ref="text" />
+				<input type="submit" value="Post" />
+			</form>			
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox data={data} />, 
+	<CommentBox url="/api/comments" pollInterval={2000} />, 
 	document.getElementById('content')
 );
